748141f6df61e05eabe8d3f21c9ce881b9c606a0
Alles met Programmeren <51288664+Alles-met-Programmeren@users.noreply.github.com>
2019-06-03 07:49:56 +0200

    Een bewegende blok
    de 'w' en 's' zijn nog niet ingestelt

--- Minecraft/sketch.js
+++ Minecraft/sketch.js
@@ -1,9 +1,27 @@
+let pos;
+
 function setup() {
   createCanvas(400, 400);
+  pos = createVector(0,240);
 }
 
 function draw() {
   background(0,200,255);
+  if(keyIsPressed) {
+   if(key == 'a') {
+     pos.x -= 1;
+   }
+    if(key == 'd') {
+      pos.x += 1;
+    }
+    if(key == 'w') {
+  
+    }
+    if(key == 's') {
+      
+    }
+  }
+  rect(pos.x,pos.y,40,40);
   for(let i = 0;i < 10;i++) {
    for(let j = 0;j < 3;j++) {
      rect(i*40,j*40+280,40,40);
